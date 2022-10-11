dd580d2f5b64a0b5e43393020adb285b313906d7
GustavoSNavi <gustavo.navi@kavum.com.br>
2022-10-11 17:37:25 -0300

    Populado informações da página Home

--- src/ezClinik/pages/ezclinikHome/index.tsx
+++ src/ezClinik/pages/ezclinikHome/index.tsx
@@ -3,7 +3,16 @@ import { useSelector } from "react-redux";
 import Text from "../../Components/Text";
 import { selectLoggedUser } from "../../store/redux/user/userSlice";
 import currentTimeSalutation from "../../utils/getCurrentTime";
-import { StyBody, StyContainer, StyHeader, StyTitle } from "./styles";
+import {
+  StyBody,
+  StyContainer,
+  StyHeader,
+  StyPermissionBody,
+  StyPermissionContainer,
+  StyPermissionInfos,
+  StyPermissionType,
+  StyTitle,
+} from "./styles";
 
 /**
  * @description Home Page.
@@ -11,6 +20,14 @@ import { StyBody, StyContainer, StyHeader, StyTitle } from "./styles";
  */
 export const EZClinikHome: React.FC<{}> = () => {
   const loggedUser = useSelector(selectLoggedUser);
+
+  const roleName: any = {
+    MANAGER: "Gerente",
+    DOCTOR: "Profissional da Clínica",
+    PATIENT: "Paciente",
+  };
+
+  console.log("loggedUser: ", loggedUser);
 
   return (
     <>
@@ -22,9 +39,82 @@ export const EZClinikHome: React.FC<{}> = () => {
               size="24px"
               value={`${currentTimeSalutation()}, ${loggedUser.name}`}
             />
+            <div>
+              <Text size="20px" value="Tipo de Acesso: " />
+              <Text
+                fontWeight="600"
+                size="20px"
+                value={roleName[String(loggedUser.naUserType)]}
+              />
+            </div>
           </StyTitle>
         </StyHeader>
-        <StyBody></StyBody>
+        <StyBody>
+          <StyPermissionContainer>
+            <StyPermissionBody>
+              <StyPermissionType>
+                {loggedUser.naUserType === "PATIENT" && (
+                  <>
+                    <Text
+                      size="18px"
+                      value={`Aqui no EZClinik, você poderá acessar as páginas: "Home", "Agendar Consulta" e "Minhas Consultas".`}
+                    />
+                    <StyPermissionInfos>
+                      <Text
+                        size="18px"
+                        value={`Na página "Agendar Consulta", você conseguirá agendar sua consulta com um profissional de uma determinada clínica.`}
+                      />
+                      <Text
+                        size="18px"
+                        value={`Nas página "Minhas Consultas", você conseguirá visualizar suas consultas agendadas.`}
+                      />
+                    </StyPermissionInfos>
+                  </>
+                )}
+                {loggedUser.naUserType === "DOCTOR" && (
+                  <>
+                    <Text
+                      size="18px"
+                      value={`Aqui no EZClinik, você poderá acessar as páginas: "Home", "Meu Calendário" e "Próximas Consultas".`}
+                    />
+                    <StyPermissionInfos>
+                      <Text
+                        size="18px"
+                        value={`Na página "Meu Calendário", você conseguirá determinar os dias e horários disponíveis para os pacientes agendarem suas consultas.`}
+                      />
+                      <Text
+                        size="18px"
+                        value={`Na página "Próximas Consultas", você conseguirá visualizar suas próximas consultas de acordo com o dia selecionado.`}
+                      />
+                    </StyPermissionInfos>
+                  </>
+                )}
+                {loggedUser.naUserType === "MANAGER" && (
+                  <>
+                    <Text
+                      size="18px"
+                      value={`Aqui no EZClinik, você poderá acessar as páginas: "Home", "Profissionais da Clínica", "Histórico de Consultas" e "Minha Clínica".`}
+                    />
+                    <StyPermissionInfos>
+                      <Text
+                        size="18px"
+                        value={`Na página "Profissionais da Clínica", você conseguirá cadastrar, editar, deletar e visualizar os profissionais cadastrados na sua clínica.`}
+                      />
+                      <Text
+                        size="18px"
+                        value={`Na página "Histórico de Consultas", você conseguirá visualizar todas as consutas realizadas pelos profissionais cadastrados na sua clínica.`}
+                      />
+                      <Text
+                        size="18px"
+                        value={`Na página "Minha Clínica", você conseguirá cadastrar ou editar os dados da sua clínica.`}
+                      />
+                    </StyPermissionInfos>
+                  </>
+                )}
+              </StyPermissionType>
+            </StyPermissionBody>
+          </StyPermissionContainer>
+        </StyBody>
       </StyContainer>
     </>
   );
--- src/ezClinik/pages/ezclinikHome/styles.ts
+++ src/ezClinik/pages/ezclinikHome/styles.ts
@@ -13,16 +13,53 @@ export const StyHeader = styled.div`
   padding-bottom: 10px;
   height: 50px;
   width: 100%;
-  margin-bottom: 50px;
 `;
 
 export const StyTitle = styled.div`
   display: flex;
   flex-direciton: row;
   justify-content: space-between;
+  flex-flow: wrap;
 `
 
 export const StyBody = styled.div`
 width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
+
+export const StyPermissionContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyPermissionText = styled.div`
+  display: flex;
+  align-items: flex-start;
+  width: 50%;
+  margin: 20px 10px;
+  border-bottom: 1px solid #000; 
+`
+
+export const StyPermissionBody = styled.div`
+  margin: 20px 10px;
+`
+
+export const StyPermissionType = styled.div`
+  margin-bottom: 60px;
+`
+
+export const StyPermissionInfos = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  margin-left: 20px;
+
+  & > span {
+    margin-bottom: 10px;
+    display: list-item;
+  }
+`
